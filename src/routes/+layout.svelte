105c4385a93c984de964079a663a0a74977d92a3
bennymi <xbenmmusicx@gmail.com>
2023-03-19 19:32:17 +0100

    move buttons to separate route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,6 @@
 </script>
 
 <div class:dark>
-	<button class="btn-primary" on:click={() => (dark = !dark)}>Dark/Light</button>
-	<button class="btn-gradient-br-primary-200-tertiary-200" on:click={() => (dark = !dark)}
-		>Dark/Light</button
-	><button class="btn-gradient-tr-primary-tertiary-secondary">Click</button>
-	<button class="btn-glass-primary">Click</button>
-	<button class="bg-teal-500/40">Click</button>
-	<button class="bg-primary-500/40">Click</button>
 	<button class="btn-gradient-b-primary-secondary" on:click={() => (dark = !dark)}
 		>Dark/Light</button
 	>
---
+++ src/routes/buttons/+page.svelte
@@ -0,0 +1,8 @@
+<button class="btn-primary">Dark/Light</button>
+<button class="btn-gradient-br-primary-200-tertiary-200">Dark/Light</button><button
+	class="btn-gradient-tr-primary-tertiary-secondary">Click</button
+>
+<button class="btn-glass-primary">Click</button>
+<button class="bg-teal-500/40">Click</button>
+<button class="bg-primary-500/40">Click</button>
+<button class="btn-gradient-b-primary-secondary">Dark/Light</button>
