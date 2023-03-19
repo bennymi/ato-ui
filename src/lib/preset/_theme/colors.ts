86102f5af59a8e1251e5c7fa5163dc873f1d2118
bennymi <xbenmmusicx@gmail.com>
2023-03-19 11:36:00 +0100

    add color preset

--- src/lib/preset/_theme/colors.ts
+++ src/lib/preset/_theme/colors.ts
@@ -3,10 +3,10 @@ import { colorValues } from "../../types/colors"
 
 export const colors = {
     primary: Object.fromEntries(colorValues.map(x => [x, `var(--color-primary-${x})`])),
-      secondary: Object.fromEntries(colorValues.map(x => [x, `var(--color-secondary-${x})`])),
-      tertiary: Object.fromEntries(colorValues.map(x => [x, `var(--color-tertiary-${x})`])),
-      success: Object.fromEntries(colorValues.map(x => [x, `var(--color-success-${x})`])),
-      warning: Object.fromEntries(colorValues.map(x => [x, `var(--color-warning-${x})`])),
-      error: Object.fromEntries(colorValues.map(x => [x, `var(--color-error-${x})`])),
-      surface: Object.fromEntries(colorValues.map(x => [x, `var(--color-surface-${x})`])),
+    secondary: Object.fromEntries(colorValues.map(x => [x, `var(--color-secondary-${x})`])),
+    tertiary: Object.fromEntries(colorValues.map(x => [x, `var(--color-tertiary-${x})`])),
+    success: Object.fromEntries(colorValues.map(x => [x, `var(--color-success-${x})`])),
+    warning: Object.fromEntries(colorValues.map(x => [x, `var(--color-warning-${x})`])),
+    error: Object.fromEntries(colorValues.map(x => [x, `var(--color-error-${x})`])),
+    surface: Object.fromEntries(colorValues.map(x => [x, `var(--color-surface-${x})`]))
 } satisfies Theme['colors']
