4cec565ab98348358cf3cf989ec83c822151c011
bennymi <xbenmmusicx@gmail.com>
2023-03-21 16:17:27 +0100

    add button glass gradients

--- src/lib/preset/_rules/buttons.ts
+++ src/lib/preset/_rules/buttons.ts
@@ -5,6 +5,11 @@ import { directionsJoined } from '../../types/directions.d';
 
 const baseStyles = 'px-3 py-1 inline-flex justify-center items-center font-medium space-x-2 rounded-token-base disabled:(opacity-80 cursor-not-allowed)';
 
+const gradientStyles = 'hover:brightness-105';
+
+const glassStyles = 'ring-1';
+const glassOp = '40';
+
 export const shortcuts: Shortcut[] = [
     // Button regular
     [
@@ -17,31 +22,56 @@ export const shortcuts: Shortcut[] = [
     
     // Button glass
     [
-        new RegExp(`^btn-glass-(${themeColorsJoined})$`), 
-        ([, b]) => `${baseStyles} bg-${b}-500/30 text-on-${b} ring-1 ring-${b}-500 hover:bg-${b}-600 disabled:hover:bg-${b}-500`, 
+        new RegExp(`^btn-glass-(${themeColorsJoined})-?(${colorValuesJoined})?$`), 
+        ([, b, v]) => `${baseStyles} bg-${b}-${ v ?? '500'}/${glassOp} text-on-${b} ${glassStyles} ring-${b}-${ v ?? '500'} hover:bg-${b}-${ v ?? '500'} disabled:hover:bg-${b}-${ v ?? '500'}/${glassOp}`, 
         {
-            autocomplete: [`btn-glass-(${themeColorsJoined})`]
+            autocomplete: [`btn-glass-(${themeColorsJoined})`, `btn-glass-(${themeColorsJoined})-(${colorValuesJoined})`]
+        }
+    ],
+    [
+        new RegExp(`^btn-glass-(${directionsJoined})-(${themeColorsJoined})-?(${colorValuesJoined})?-(${themeColorsJoined})-?(${colorValuesJoined})?$`), 
+        ([, dir, b1, v1, b2, v2]) => 
+            `${baseStyles} bg-gradient-to-${dir} from-${b1}-${ v1 ?? '500'}/${glassOp} to-${b2}-${v2 ?? '500'}/${glassOp} 
+            text-on-${b1} ${glassStyles} ring-${b1}-${ v1 ?? '500'} 
+            hover:(from-${b1}-${ v1 ?? '500'} to-${b2}-${v2 ?? '500'}) 
+            disabled:hover:(from-${b1}-${ v1 ?? '500'}/${glassOp} to-${b2}-${v2 ?? '500'}/${glassOp})`, 
+        {
+            autocomplete: [`btn-glass-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined})`, `btn-glass-(${directionsJoined})-(${themeColorsJoined})-(${colorValuesJoined})-(${themeColorsJoined})-(${colorValuesJoined})`]
+        }
+    ],
+    [
+        new RegExp(`^btn-glass-(${directionsJoined})-(${themeColorsJoined})-?(${colorValuesJoined})?-(${themeColorsJoined})-?(${colorValuesJoined})?-(${themeColorsJoined})-?(${colorValuesJoined})?$`), 
+        ([, dir, b1, v1, b2, v2, b3, v3]) => 
+            `${baseStyles} bg-gradient-to-${dir} from-${b1}-${ v1 ?? '500'}/${glassOp} via-${b2}-${v2 ?? '500'}/${glassOp} to-${b3}-${v3 ?? '500'}/${glassOp}
+            text-on-${b1} ${glassStyles} ring-${b1}-${ v1 ?? '500'} 
+            hover:(from-${b1}-${ v1 ?? '500'} via-${b2}-${v2 ?? '500'} to-${b3}-${v3 ?? '500'}) 
+            disabled:hover:(from-${b1}-${ v1 ?? '500'}/${glassOp} via-${b2}-${v2 ?? '500'}/${glassOp} to-${b3}-${v3 ?? '500'}/${glassOp})`, 
+        {
+            autocomplete: [
+                `btn-glass-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined}-(${themeColorsJoined}))`, 
+                `btn-glass-(${directionsJoined})-(${themeColorsJoined})-(${colorValuesJoined})-(${themeColorsJoined})-(${colorValuesJoined})-(${themeColorsJoined})-(${colorValuesJoined})`
+            ]
         }
     ],
 
     // Button gradients
     [
         new RegExp(`^btn-gradient-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined})-(${themeColorsJoined})$`), 
-        ([, dir, b1, b2, b3]) => `${baseStyles} bg-gradient-to-${dir} text-on-${b1} from-${b1}-500 via-${b2}-500 to-${b3}-500 hover:brightness-110`, 
+        ([, dir, b1, b2, b3]) => `${baseStyles} bg-gradient-to-${dir} text-on-${b1} from-${b1}-500 via-${b2}-500 to-${b3}-500 ${gradientStyles}`, 
         {
             autocomplete: [`btn-gradient-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined})-(${themeColorsJoined})`]
         }
     ],
     [
         new RegExp(`^btn-gradient-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined})$`), 
-        ([, dir, b1, b2]) => `${baseStyles} bg-gradient-to-${dir} text-on-${b1} from-${b1}-500 to-${b2}-500 hover:brightness-110`, 
+        ([, dir, b1, b2]) => `${baseStyles} bg-gradient-to-${dir} text-on-${b1} from-${b1}-500 to-${b2}-500 ${gradientStyles}`, 
         {
             autocomplete: [`btn-gradient-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined})`]
         }
     ],
     [
         new RegExp(`^btn-gradient-(${directionsJoined})-(${themeColorsJoined})-?(${colorValuesJoined})?-(${themeColorsJoined})-(${colorValuesJoined})?$`), 
-        ([, dir, b1, v1, b2, v2]) => `${baseStyles} bg-gradient-to-${dir} text-on-${b1} from-${b1}${v1 ? `-${v1}` : '-500'} to-${b2}${v2 ? `-${v2}` : '-500'} hover:brightness-110`, 
+        ([, dir, b1, v1, b2, v2]) => `${baseStyles} bg-gradient-to-${dir} text-on-${b1} from-${b1}${v1 ? `-${v1}` : '-500'} to-${b2}${v2 ? `-${v2}` : '-500'} ${gradientStyles}`, 
         {
             autocomplete: [`btn-gradient-(${directionsJoined})-(${themeColorsJoined})-(${themeColorsJoined})`]
         }
--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -5,6 +5,9 @@
 	<button class="btn-gradient-tr-primary-tertiary-secondary">Click</button>
 	<button class="btn-glass-primary">Click</button>
 	<button class="btn-glass-secondary">Glass Primary</button>
+	<button class="btn-glass-secondary" disabled>Glass Disabled</button>
+	<button class="btn-glass-tl-tertiary-primary">Glass Gradient</button>
+	<button class="btn-glass-tl-tertiary-primary-secondary">Glass 3 Gradient</button>
 	<button class="btn-gradient-b-primary-secondary">Dark/Light</button>
 	<button class="btn-gradient-br-primary-400-secondary-800">Dark/Light</button>
 </div>
