ddd22a74a2a3b6e9d9dab97e745d2e7ef3ec21c5
bennymi <xbenmmusicx@gmail.com>
2023-03-22 21:41:05 +0100

    add button descriptions and change some variable names

--- src/lib/preset/_rules/text.ts
+++ src/lib/preset/_rules/text.ts
@@ -1,6 +1,6 @@
 import type { Shortcut } from '@unocss/core';
 
-import { themeColorsJ, colorValuesJ } from '../../types/colors.d';
+import { themeColorsJ, shadesJ } from '../../types/colors.d';
 import { directionsJ } from '../../types/directions.d';
 
 const gradientClasses = 'bg-clip-text text-transparent';
@@ -17,25 +17,25 @@ export const textSCs: Shortcut[] = [
 
     // Text with opacity values
     // [
-    //     new RegExp(`^text-(${themeColorsJ})-(${colorValuesJ})(/[1-9][0-9]?|100)?$`), 
+    //     new RegExp(`^text-(${themeColorsJ})-(${shadesJ})(/[1-9][0-9]?|100)?$`), 
     //     ([, t, c, d]) => `text-${t}-${c}${d ? ' opacity-[' + parseInt(d.substring(1)) / 100 + ']' : ''}`, 
     //     {
-    //         autocomplete: [`text-(${themeColorsJ})-(${colorValuesJ})`]
+    //         autocomplete: [`text-(${themeColorsJ})-(${shadesJ})`]
     //     }
     // ],
 
     // Text
     [
-        new RegExp(`^ato-text-(${themeColorsJ})-(${colorValuesJ})-(${colorValuesJ})$`), 
+        new RegExp(`^ato-text-(${themeColorsJ})-(${shadesJ})-(${shadesJ})$`), 
         ([, b, v1, v2]) => `text-${b}-${v1} dark:text-${b}-${v2}`,
         {
-            autocomplete: `ato-text-(${themeColorsJ})-(${colorValuesJ})-(${colorValuesJ})`
+            autocomplete: `ato-text-(${themeColorsJ})-(${shadesJ})-(${shadesJ})`
         }
     ],
 
     // Text Inverse
     [
-        new RegExp(`^ato-text-inverse-(${themeColorsJ})(-${colorValuesJ})?-(${themeColorsJ})(-${colorValuesJ})?$`), 
+        new RegExp(`^ato-text-inverse-(${themeColorsJ})(-${shadesJ})?-(${themeColorsJ})(-${shadesJ})?$`), 
         ([, b1, v1, b2, v2]) => `text-${b1}${['white', 'black'].includes(b1) ? '' : v1 ?? '-500'} dark:text-${b2}${['white', 'black'].includes(b2) ? '' : v2 ?? '-500'}`,
         {
             // autocomplete: `ato-text-inverse-(${themeColorsJ})-(${themeColorsJ})`
@@ -45,14 +45,14 @@ export const textSCs: Shortcut[] = [
 
     // Text gradients
     [
-        new RegExp(`^text-gradient-(${directionsJ})-(${themeColorsJ})-?(${colorValuesJ})?-(${themeColorsJ})-?(${colorValuesJ})?-(${themeColorsJ})-?(${colorValuesJ})?$`), 
+        new RegExp(`^text-gradient-(${directionsJ})-(${themeColorsJ})-?(${shadesJ})?-(${themeColorsJ})-?(${shadesJ})?-(${themeColorsJ})-?(${shadesJ})?$`), 
         ([, dir, b1, v1, b2, v2, b3, v3]) => `${gradientClasses} bg-gradient-to-${dir} from-${b1}-${v1 ?? '500'} via-${b2}-${v2 ?? '500'} to-${b3}-${ v3 ?? '500'}`, 
         {
             autocomplete: [`text-gradient-(${directionsJ})-(${themeColorsJ})-(${themeColorsJ})-(${themeColorsJ})`]
         }
     ],
     [
-        new RegExp(`^text-gradient-(${directionsJ})-(${themeColorsJ})-?(${colorValuesJ})?-(${themeColorsJ})-?(${colorValuesJ})?$`), 
+        new RegExp(`^text-gradient-(${directionsJ})-(${themeColorsJ})-?(${shadesJ})?-(${themeColorsJ})-?(${shadesJ})?$`), 
         ([, dir, b1, v1, b2, v2]) => `${gradientClasses} bg-gradient-to-${dir} from-${b1}-${v1 ?? '500'} to-${b2}-${v2 ?? '500'}`, 
         {
             autocomplete: [`text-gradient-(${directionsJ})-(${themeColorsJ})-(${themeColorsJ})`]
