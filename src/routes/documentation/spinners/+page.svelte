a0095c167d4b05ec002c216fc3947726525bdb3e
bennymi <xbenmmusicx@gmail.com>
2023-03-25 23:06:42 +0100

    add variant of regular spinner

--- src/routes/documentation/spinners/+page.svelte
+++ src/routes/documentation/spinners/+page.svelte
@@ -2,6 +2,7 @@
 	<div class="flex justify-center items-center gap-x-32 gap-y-20 flex-wrap mx-1/3">
 		<!-- Regular circle -->
 		<span class="s-circle-primary" />
+		<span class="s-circle-primary border-t-transparent" />
 		<span class="s-circle-secondary border-dotted border-10" />
 		<span class="s-circle-tertiary-800 border-dashed border-6" />
 		<span class="s-circle-tertiary-800 border-dashed w-8 h-8" />
