877bd4bfce90b587f0884135b5db7ec230dd9d8f
bennymi <xbenmmusicx@gmail.com>
2023-04-01 19:07:36 +0200

    add badge positions

--- src/lib/components/avatar/Avatar.svelte
+++ src/lib/components/avatar/Avatar.svelte
@@ -2,19 +2,41 @@
 	import { getContext } from 'svelte';
 	import type { Rounded, Position } from '../../types/types.d';
 
-	export let size: string = 'w-16 h-16';
+	export let size: string = 'w-16'; //  h-16
 	export let rounded: Rounded = 'rounded-token-base';
 	export let badge: boolean = false;
 	export let badgeBackground: string = 'bg-green-500';
 	export let badgePosition: Position = 'bottom-right';
 	export let badgeIcon: string = '';
 	export let typing: boolean = false;
-	export let typingSpinner: string = 'primary-100';
+	export let typingSpinner: string = 's-dots-primary-100';
 
 	// Test getContext without a parent
 	// $: console.log('context', getContext('loading'));
 
-	$: badgePos = rounded === 'rounded-full' ? 'bottom-1 right-1' : 'bottom-0 right-0';
+	let positions: any = {
+		'bottom-right': {
+			'rounded-full': 'bottom-0.5 right-0.5',
+			other: 'bottom-0 right-0'
+		},
+		'bottom-left': {
+			'rounded-full': 'bottom-0.5 left-0.5',
+			other: 'bottom-0 left-0'
+		},
+		'top-right': {
+			'rounded-full': 'top-0.5 right-0.5',
+			other: 'top-0 right-0'
+		},
+		'top-left': {
+			'rounded-full': 'top-0.5 left-0.5',
+			other: 'top-0 left-0'
+		}
+	};
+
+	$: badgePos =
+		rounded === 'rounded-full'
+			? positions[badgePosition]['rounded-full']
+			: positions[badgePosition]['other'];
 </script>
 
 <div class="{size} {rounded} relative inline-flex justify-center items-center">
@@ -30,9 +52,9 @@
 	</div>
 	{#if typing}
 		<span
-			class="absolute {badgePos} {badgeBackground} w-10 rounded-token-base border-2 border-surface-800 flex justify-center items-center"
+			class="absolute {badgePos} {badgeBackground} w-8 h-4 rounded-token-base border-2 border-surface-800 flex justify-center items-center"
 		>
-			<span class="inline-flex s-dots-{typingSpinner} scale-[0.4]" />
+			<span class="inline-flex {typingSpinner} scale-[0.3]" />
 		</span>
 	{/if}
 	{#if badge && !typing}
--- src/routes/documentation/avatar/+page.svelte
+++ src/routes/documentation/avatar/+page.svelte
@@ -5,8 +5,9 @@
 </script>
 
 <div class="min-h-screen flex flex-col justify-center items-center">
-	<button class="btn-bl-success-secondary-warning my-10" on:click={() => (typing = !typing)}
-		>Typing On/Off</button
+	<button
+		class="btn-border-surface-900-bl-success-secondary-tertiary my-10 btn-lg"
+		on:click={() => (typing = !typing)}>Typing On/Off</button
 	>
 	<div class="flex flex-wrap justify-center items-center gap-8">
 		<!-- <Avatar size="w-20 h-20" rounded="rounded-none" />
@@ -15,8 +16,11 @@
 		<Avatar size="w-20 h-20" rounded="rounded-3xl" />
 		<Avatar size="w-20 h-20" rounded="rounded-full" /> -->
 		<Avatar rounded="rounded-none" badgeBackground="bg-secondary-500" badge />
-		<Avatar badge />
-		<Avatar badge badgeBackground="bg-warning-500" {typing} typingSpinner="surface-900" />
+		<Avatar badge badgePosition="bottom-left" />
+		<Avatar badge rounded="rounded-full" badgePosition="bottom-left" />
+		<Avatar badge badgePosition="top-left" />
+		<Avatar badge rounded="rounded-full" badgePosition="top-right" {typing} />
+		<Avatar badge badgeBackground="bg-warning-500" {typing} typingSpinner="s-dots-surface-900" />
 		<Avatar
 			rounded="rounded-xl"
 			badgeBackground="bg-red-600"
