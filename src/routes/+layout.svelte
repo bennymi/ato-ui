30365bc81d5b2f9763c363feb6475eb9fb36470b
bennymi <xbenmmusicx@gmail.com>
2023-04-07 17:53:04 +0200

    refactor background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class:dark>
-	<div class="ato-bg-inverse-white-surface-900 min-h-screen">
+	<div class="bg-inverse-white-surface-900 min-h-screen">
 		<button class="btn-b-primary-secondary" on:click={() => (dark = !dark)}>Dark/Light</button>
 		<slot />
 	</div>
