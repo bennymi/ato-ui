c4b720dcc4924a1e10edcb1f38efd2dfe667a798
bennymi <xbenmmusicx@gmail.com>
2023-05-16 16:23:30 +0200

    add sliders

--- src/routes/designer/Contrasts.svelte
+++ src/routes/designer/Contrasts.svelte
@@ -2,6 +2,9 @@
 	import type { FullTheme, ThemeColor } from './types.d';
 	import { shade_values, theme_colors } from './constants';
 
+	import RangeSlider from 'svelte-range-slider-pips';
+	import { slide } from 'svelte/transition';
+
 	export let shades: FullTheme;
 
 	let bg_colors: { [key in ThemeColor]: string[] } = {
@@ -109,6 +112,7 @@
 	// };
 
 	let contrast_threshold = 21;
+	let show_slider = '';
 </script>
 
 <div class="flex gap-2 my-2">
@@ -127,22 +131,62 @@
 </div>
 
 <div class="flex flex-col gap-1">
+	<div class="flex gap-1">
+		{#each shade_values as shade}
+			<span class="w-12 text-center font-bold">{shade}</span>
+		{/each}
+	</div>
 	{#each theme_colors as palette}
-		<div class="flex gap-1 justify-center items-center">
-			{#each shade_values as shade}
-				{@const { contrast, color, onColor } = shades[palette][shade]}
+		<div class="flex flex-col">
+			<button
+				class="flex gap-1 justify-center items-center"
+				on:click={() => (show_slider = show_slider === palette ? '' : palette)}
+				aria-expanded={show_slider === palette}
+				aria-controls="slider-{palette}"
+			>
+				{#each shade_values as shade}
+					{@const { contrast, color, onColor } = shades[palette][shade]}
+					<div
+						class="group flex justify-center items-center cursor-pointer h-12 w-12 rounded-token-container transition-all duration-400 {contrast >=
+						contrast_threshold
+							? 'scale-80 opacity-50'
+							: ''}"
+						style="background: {color};"
+					>
+						<span class="transition-all group-hover:(scale-120)" style="color: {onColor};">
+							{contrast.toFixed(2)}
+						</span>
+					</div>
+				{/each}
+			</button>
+			{#if show_slider === palette}
 				<div
-					class="group flex justify-center items-center cursor-pointer h-12 w-12 rounded-token-container transition-all duration-400 {contrast >=
-					contrast_threshold
-						? 'scale-80 opacity-50'
-						: ''}"
-					style="background: {color};"
+					id="slider-{palette}"
+					class="mb-4 mt-5 flex gap-12"
+					transition:slide={{ duration: 250 }}
 				>
-					<span class="transition-all group-hover:(scale-120)" style="color: {onColor};">
-						{contrast.toFixed(2)}
-					</span>
+					<div class="w-65">
+						<RangeSlider
+							values={[50, 60, 70]}
+							--range-slider={shades[palette][200].color}
+							--range-handle-inactive={shades[palette][300].color}
+							--range-handle-focus={shades[palette][600].color}
+							--range-handle={shades[palette][400].color}
+						/>
+						<div class="font-semibold mt-2">Lighten</div>
+					</div>
+					<div class="w-65">
+						<RangeSlider
+							values={[50, 60, 70]}
+							--range-slider={shades[palette][200].color}
+							--range-handle-inactive={shades[palette][300].color}
+							--range-handle-focus={shades[palette][600].color}
+							--range-handle={shades[palette][400].color}
+						/>
+						<div class="font-semibold mt-2">Darken</div>
+					</div>
 				</div>
-			{/each}
+			{/if}
 		</div>
 	{/each}
 </div>
@@ -207,3 +251,9 @@
 		{/each}
 	</div>
 </div> -->
+
+<style>
+	:global(.rangeSlider) {
+		margin: 0;
+	}
+</style>
