dcbaa807a96951feceac0b20f1578ded235353d8
bennymi <xbenmmusicx@gmail.com>
2023-06-08 15:30:01 +0200

    update component docs pages to have a styled and header link

---
+++ src/routes/docs/components/[slug]/+layout.server.ts
@@ -0,0 +1,14 @@
+
+export async function load({ params }) {
+
+    const glob_components = import.meta.glob('/src/docs/components/*.md');
+
+    let components: string[] = Object.keys(glob_components).filter((v) => v.includes(params.slug.replace('-headless', '')));
+
+    components = <string[]>components.map((v) => v.split('/').at(-1)?.split('.')[0]);
+    
+	return {
+        styledExists: components.includes(params.slug.replace('-headless', '')),
+        headlessExists: components.includes(`${params.slug.replace('-headless', '')}-headless`)
+	}
+}
---
+++ src/routes/docs/components/[slug]/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let data;
+
+	let { styledExists, headlessExists } = data;
+
+	$: pageIsHeadless = $page.url.pathname.includes('-headless');
+</script>
+
+{#if styledExists && headlessExists}
+	<nav
+		class="flex justify-center items-center w-full mb-4 [&>a]:(min-w-40 flex justify-center items-center gap-1 text-xl font-bold px-2 py-1 transition-all duration-150)"
+	>
+		<a
+			href="/docs/components/{$page.url.pathname.replace('-headless', '')}"
+			class="rounded-l-btn {pageIsHeadless ? 'surface-500 hover:primary-500/70' : 'primary-500'}"
+		>
+			<span class="i-material-symbols-water-drop-rounded" />
+			<span>Styled</span>
+		</a>
+		<a
+			href="/docs/components/{$page.url.pathname.replace('-headless', '')}-headless"
+			class="rounded-r-btn {pageIsHeadless ? 'primary-500' : 'surface-500 hover:primary-500/70'}"
+		>
+			<span class="i-material-symbols-water-drop-outline-rounded" />
+			<span>Headless</span>
+		</a>
+	</nav>
+{/if}
+
+<slot />
