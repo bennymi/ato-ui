134af1e390d9cd24f8c68e7d6afb2c29715603d7
bennymi <xbenmmusicx@gmail.com>
2023-07-04 10:51:46 +0200

    git status

--- src/routes/docs/components/[slug]/+page.svelte
+++ src/routes/docs/components/[slug]/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+
 	export let data;
+
+	let unavailableStyle = 'text-surface-500-100 opacity-50 cursor-not-allowed';
+	let inactiveStyle = 'text-surface-800-100 hover:primary-500/80';
+
+	$: pageIsHeadless = $page.url.pathname.includes('-headless');
+	$: styledPath = data.styledExists ? $page.url.pathname.replace('-headless', '') : '';
+	$: headlessPath = data.headlessExists ? `${$page.url.pathname.replace('-headless', '')}-headless` : '';
 </script>
 
 <svelte:head>
@@ -7,5 +16,24 @@
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
 </svelte:head>
+
+<nav
+	class="flex items-center mb-4 rounded-btn border-1 border-primary-500 w-fit [&>a]:(flex justify-center items-center gap-1 text-xl font-bold px-2 py-1 transition-all duration-150)"
+>
+	<a
+		href={data.headlessExists ? headlessPath : '#'}
+		class="rounded-l-btn {!data.headlessExists ? unavailableStyle : pageIsHeadless ? 'primary-500' : inactiveStyle}"
+	>
+		<span class="sr-only">Headless component</span>
+		<span class="i-material-symbols-water-drop-outline-rounded" />
+	</a>
+	<a
+		href={data.styledExists ? styledPath : '#'}
+		class="rounded-r-btn {!data.styledExists ? unavailableStyle : pageIsHeadless ? inactiveStyle: 'primary-500'}"
+	>
+		<span class="sr-only">Styled component</span>
+		<span class="i-material-symbols-water-drop-rounded" />
+	</a>
+</nav>
 
 <svelte:component this={data.content} />
--- src/routes/docs/components/[slug]/+page.ts
+++ src/routes/docs/components/[slug]/+page.ts
@@ -5,10 +5,18 @@ export const load = (async ({ params }) => {
 	try {
 		const documentation = await import(/* @vite-ignore */`../../../../docs/guides/components/${params.slug}.md`);
 
+		const glob_components = import.meta.glob('/src/docs/guides/components/*.md');
+
+		let components: string[] = Object.keys(glob_components).filter((v) => v.includes(params.slug.replace('-headless', '')));
+
+		components = <string[]>components.map((v) => v.split('/').at(-1)?.split('.')[0]);
+
 		return {
 			slug: params.slug,
 			content: documentation.default,
 			meta: documentation.metadata,
+			styledExists: components.includes(params.slug.replace('-headless', '')),
+			headlessExists: components.includes(`${params.slug.replace('-headless', '')}-headless`)
 		};
 	} catch (e) {
 		throw error(404, `Could not find ${params.slug}`);
