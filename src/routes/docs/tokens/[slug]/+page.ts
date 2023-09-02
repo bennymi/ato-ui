22ad8d29dc41782d0658ea99872d5f99416fb93a
bennymi <xbenmmusicx@gmail.com>
2023-09-02 10:17:24 +0200

    update +page of tokens page

--- src/routes/docs/tokens/[slug]/+page.ts
+++ src/routes/docs/tokens/[slug]/+page.ts
@@ -1,15 +1,38 @@
-import { error } from '@sveltejs/kit'
-import type { PageLoad } from './$types';
+// import { error } from '@sveltejs/kit'
+// import type { PageLoad } from './$types';
+
+// export const load = (async ({ params }) => {
+// 	try {
+// 		const documentation = await import(/* @vite-ignore */`../../../../docs/guides/tokens/${params.slug}.md`)
+
+// 		return {
+// 			content: documentation.default,
+// 			meta: documentation.metadata
+// 		}
+// 	} catch (e) {
+// 		throw error(404, `Could not find ${params.slug}`)
+// 	}
+// }) satisfies PageLoad;
+
+import { error } from '@sveltejs/kit';
+import type { PageLoad, EntryGenerator } from './$types';
+import { getDocsData, getGuidesFolderFiles } from '$docs/utils/preview';
+
+export const entries = (() => {
+	const slugs = getGuidesFolderFiles('tokens').map((file) => ({ slug: file }));
+	return slugs;
+}) satisfies EntryGenerator;
 
 export const load = (async ({ params }) => {
-	try {
-		const documentation = await import(/* @vite-ignore */`../../../../docs/guides/tokens/${params.slug}.md`)
+    const { slug } = params;
 
-		return {
-			content: documentation.default,
-			meta: documentation.metadata
-		}
-	} catch (e) {
-		throw error(404, `Could not find ${params.slug}`)
-	}
+	if (!getGuidesFolderFiles('tokens').includes(slug)) {
+        throw error(404);
+    }
+
+    const docsComponentData = await getDocsData({ slug, isComponent: false });
+
+    return {
+        ...docsComponentData,
+    };
 }) satisfies PageLoad;
