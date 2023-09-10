bfd35bce4173daa27b7171034cf2c3aafc01c1c0
Konstantinos Kostarellis <dev@kostarellis.com>
2023-09-10 16:26:17 +0200

    deps updated + moved deps into devdependencies
    - minor: justify-left -> justify-start

--- src/docs/previews/accordion/colors/app.svelte
+++ src/docs/previews/accordion/colors/app.svelte
@@ -1,59 +1,64 @@
 <script lang="ts">
-    import { Accordion } from 'ato-ui';
+	import { Accordion } from 'ato-ui';
 
-    type ExampleItem = {
-        key: string;
-        title: string;
-        content: string;
-        icon: string;
-        titleBgStyle: string;
-    }
+	type ExampleItem = {
+		key: string;
+		title: string;
+		content: string;
+		icon: string;
+		titleBgStyle: string;
+	};
 
-    // From the Avatar wiki: https://avatar.fandom.com/wiki/Bending_arts
-     const items: ExampleItem[] = [
-        {
-            key: 'water',
-            title: 'Waterbending',
-            content: 'Waterbending is practiced by some people of the Water Tribe. A versatile element, it is unique in the sense that the original bender of the element was not an animal, but rather the Moon.',
-            icon: 'i-material-symbols-water-drop-outline-rounded',
-            titleBgStyle: 'transition-all primary-400 hover:primary-500'
-        },
-        {
-            key: 'earth',
-            title: 'Earthbending',
-            content: 'Earthbending originates in the Earth Kingdom and the first earthbenders were badgermoles, who are said to love music.',
-            icon: 'i-mdi-earth',
-            titleBgStyle: 'transition-all secondary-400 hover:secondary-500'
-        },
-        {
-            key: 'fire',
-            title: 'Firebending',
-            content: 'Firebending is used by the people of the Fire Nation and is the most aggressive bending art. Dragons were the first firebenders; they subsequently taught the Sun Warriors.',
-            icon: 'i-material-symbols-local-fire-department-rounded',
-            titleBgStyle: 'bg-gradient-br-primary-tertiary text-on-primary-500 opacity-80 hover:opacity-100'
-        },
-        {
-            key: 'air',
-            title: 'Airbending',
-            content: 'Airbending is the bending art used by the Air Nomads; the flying bison were the original airbenders.',
-            icon: 'i-mdi-weather-windy-variant',
-            titleBgStyle: 'border-1 border-surface-400/80 hover:(primary-500 border-primary-500)'
-        }
-    ];
+	// From the Avatar wiki: https://avatar.fandom.com/wiki/Bending_arts
+	const items: ExampleItem[] = [
+		{
+			key: 'water',
+			title: 'Waterbending',
+			content:
+				'Waterbending is practiced by some people of the Water Tribe. A versatile element, it is unique in the sense that the original bender of the element was not an animal, but rather the Moon.',
+			icon: 'i-material-symbols-water-drop-outline-rounded',
+			titleBgStyle: 'transition-all primary-400 hover:primary-500'
+		},
+		{
+			key: 'earth',
+			title: 'Earthbending',
+			content:
+				'Earthbending originates in the Earth Kingdom and the first earthbenders were badgermoles, who are said to love music.',
+			icon: 'i-mdi-earth',
+			titleBgStyle: 'transition-all secondary-400 hover:secondary-500'
+		},
+		{
+			key: 'fire',
+			title: 'Firebending',
+			content:
+				'Firebending is used by the people of the Fire Nation and is the most aggressive bending art. Dragons were the first firebenders; they subsequently taught the Sun Warriors.',
+			icon: 'i-material-symbols-local-fire-department-rounded',
+			titleBgStyle:
+				'bg-gradient-br-primary-tertiary text-on-primary-500 opacity-80 hover:opacity-100'
+		},
+		{
+			key: 'air',
+			title: 'Airbending',
+			content:
+				'Airbending is the bending art used by the Air Nomads; the flying bison were the original airbenders.',
+			icon: 'i-mdi-weather-windy-variant',
+			titleBgStyle: 'border-1 border-surface-400/80 hover:(primary-500 border-primary-500)'
+		}
+	];
 </script>
 
-<Accordion.Root width="w-80 md:w-96" >
-    {#each items as { title, content, key, titleBgStyle, icon }}
-        <Accordion.Item {key} {titleBgStyle} >
-            <svelte:fragment slot="title">
-                <div class="flex justify-left items-center gap-2">
-                    <span class="text-lg {icon}" />
-                    <span>{title}</span>
-                </div>
-            </svelte:fragment>
+<Accordion.Root width="w-80 md:w-96">
+	{#each items as { title, content, key, titleBgStyle, icon }}
+		<Accordion.Item {key} {titleBgStyle}>
+			<svelte:fragment slot="title">
+				<div class="flex justify-start items-center gap-2">
+					<span class="text-lg {icon}" />
+					<span>{title}</span>
+				</div>
+			</svelte:fragment>
 
-            <!-- Content in default slot -->
-            {content}
-        </Accordion.Item>
-    {/each}
+			<!-- Content in default slot -->
+			{content}
+		</Accordion.Item>
+	{/each}
 </Accordion.Root>
